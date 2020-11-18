d4a951f85408e3057d356d1d412d240686546ec8
bergpb <lindembergdepaulo@gmail.com>
2020-11-17 23:09:41 -0300

    better coding improvement

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, jsonify
-from flask_cors import CORS
-from bs4 import BeautifulSoup
-import requests
+import collections
 import json
 import os
-import collections
+
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, jsonify, g
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/numbers', methods=['GET'])
-def umbers():
-    html_doc = requests.get("http://www.resultadosmegasena.com.br/resultados-anteriores")
-    soup = BeautifulSoup(html_doc.text, "html.parser")
+__url__ = "http://www.resultadosmegasena.com.br/resultados-anteriores"
+
+
+@app.before_request
+def scrapy_data():
+    html_doc = requests.get(__url__)
+    g.soup = BeautifulSoup(html_doc.text, "html.parser")
+
+
+@app.route("/api/v1/numbers", methods=["GET"])
+def return_all_numbers():
     data = []
-    for dataBox in soup.find_all("tr", class_="rstable_td"):
+
+    for dataBox in g.soup.find_all("tr", class_="rstable_td"):
         itens = dataBox.find_all("td")
         date = itens[0].text.strip()
         info = itens[1].text.strip().split("\t\t\t\t\t\t")
@@ -24,47 +33,36 @@ def umbers():
         value = info[2].replace("Prêmio:", "").strip()
         numbers = [item.text for item in itens[2].find_all("div")]
 
-        data.append({ 
-            'date': date,
-            'concurse': concurse,
-            'winners': winners,
-            'value': value,
-            'numbers': numbers
-        })
-        
+        data.append(
+            {
+                "date": date,
+                "concurse": concurse,
+                "winners": winners,
+                "value": value,
+                "numbers": numbers,
+            }
+        )
 
-    return jsonify({'results': data})
+    return jsonify({"results": data})
 
 
-@app.route('/api/bestnumber', methods=['GET'])
-def BestNumber():
-    html_doc = requests.get("http://www.resultadosmegasena.com.br/resultados-anteriores")
-    soup = BeautifulSoup(html_doc.text, "html.parser")
-    data = []
-    number_list = []
+@app.route("/api/v1/bestnumbers", methods=["GET"])
+def return_best_numbers():
     object_list = {}
-    final_list = []
-    for dataBox in soup.find_all("tr", class_="rstable_td"):
+    number_list = final_list = []
+
+    for dataBox in g.soup.find_all("tr", class_="rstable_td"):
         itens = dataBox.find_all("td")
         number = [int(item.text) for item in itens[2].find_all("div")]
         number_list.extend(number)
 
-    for item in range(1, 60):
-        object_list.update({
-            str(item): number_list.count(item)
-        })
-        
-    sorted_list = sorted(
-        object_list.items(),
-        key = lambda x: x[1],
-        reverse = True
-    )
+    object_list.update({str(item): number_list.count(item) for item in range(1, 60)})
+    sorted_list = sorted(object_list.items(), key=lambda item: item[1], reverse=True)
+    final_list = [sorted_list[i][0] for i in range(0, 6)]
 
-    for i in range(0, 6):
-        final_list.append(sorted_list[i][0])
-    
-    return jsonify({'results': final_list})
+    return jsonify({"results": final_list})
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
